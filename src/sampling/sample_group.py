746d8593a35a675f7a6ec9d9605eff6f4a695438
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2025-09-30 03:16:09 -0400

    Add a method for pregenerating all samples in the group and filling missing values using previous samples

--- src/sampling/sample_group.py
+++ src/sampling/sample_group.py
@@ -17,5 +17,16 @@ class SampleGroup(ABC, Iterable, Sized):
     def __iter__(self) -> SampleGroupIterator:
         pass
 
+    def collapse_and_fill_down(self) -> list[dict[str, float]]:
+        current_sample = {}
+        result = []
+
+        for sample in self:
+            for key, value in sample.items():
+                current_sample[key] = value
+            result.append(current_sample.copy())
+
+        return result
+
     def __len__(self) -> int:
         return self.get_sample_count()
