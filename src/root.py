ca4c6c4b695851d6a5a027dc6ce73e515cedaead
SamAllen06 <samgallen06@gmail.com>
2026-01-27 13:44:31 -0500

    Clarified docstrings for root.py

--- src/root.py
+++ src/root.py
@@ -7,7 +7,8 @@ _SRC_PATH = Path(__file__).parent
 
 
 class ConfigPath(Enum):
-    """Define enumeration to later differentiate config paths."""
+    """Identifies which ConfigPath to get."""
+
     ROOT = auto()
     SAMPLING_PLUGINS = auto()
     ANALYSIS_PLUGINS = auto()
@@ -16,7 +17,8 @@ class ConfigPath(Enum):
 
 
 class PluginPath(Enum):
-    """Define enumeration to later differentiate plugin root."""
+    """Identifies the type of plugin to get the directory for."""
+
     SAMPLING_PLUGINS = auto()
     ANALYSIS_PLUGINS = auto()
 
@@ -28,11 +30,14 @@ _app_root: Path
 
 def set_config_root(config_root: Path) -> None:
     """
-    Define _app_root relative to the parent of the config directory, set initialized.
+    Sets the path for the configuration directory, which will be used by the rest of the
+    program. 
     
-    :param config_root: directory that contains all the configuration files used by the testing program's modules
+    :param config_root: directory that contains all the configuration files used by the 
+    testing program's modules
     :type config_root: Path
     """
+
     global _initialized
     global _config_root
     global _app_root
@@ -52,11 +57,13 @@ def set_config_root(config_root: Path) -> None:
 
 def get_app_root() -> Path:
     """
-    Ensure _app_root has been initialized and return _app_root as a Path.
+    Returns the parent of the configuration directory, which contains all the files 
+    created and modified by the project. 
     
     :return: The root of the project
     :rtype: Path
     """
+
     _require_initialization()
 
     return _app_root
@@ -64,23 +71,25 @@ def get_app_root() -> Path:
 
 def get_source_root() -> Path:
     """
-    Return the path to the Source Root
+    Returns the directory containing the source code of the program.
     
     :return: The root containing the source code of the program
     :rtype: Path
     """
+
     return _SRC_PATH
 
 
 def get_config_path(path_type: ConfigPath) -> Path:
     """
-    Compares a path type to the ConfigPath enumeration and return the matching enum member
+    Returns the requested configuration directory. 
     
-    :param path_type: A given path type
+    :param path_type: The requested configuration directory
     :type path_type: ConfigPath
-    :return: The ConfigPath enum member path_type matches
+    :return: The path to the requested configuration directory
     :rtype: Path
     """
+
     _require_initialization()
 
     match path_type:
@@ -100,13 +109,14 @@ def get_config_path(path_type: ConfigPath) -> Path:
 
 def get_plugin_root(path_type: PluginPath) -> Path:
     """
-    Compares a path type to the PluginPath enumeration and return the matching enum member
+    Returns the path to the requested plugin directory. 
     
-    :param path_type: A given path type
+    :param path_type: The requested plugin directory
     :type path_type: PluginPath
-    :return: The PluginPath enum member path_type matches
+    :return: The path to the requested plugin directory
     :rtype: Path
     """
+
     match path_type:
         case PluginPath.SAMPLING_PLUGINS:
             return _SRC_PATH / "sampling_plugins"
@@ -117,7 +127,6 @@ def get_plugin_root(path_type: PluginPath) -> Path:
 
 
 def _require_initialization() -> None:
-    """Check for if config_root is initialized"""
     if not _initialized:
         raise RuntimeError("The config_root hasn't been set yet; cannot get paths")
 
