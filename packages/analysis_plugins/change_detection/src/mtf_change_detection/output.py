7a3c16b7fdab4b28064738b182a09e86901c21ef
SamAllen06 <samgallen06@gmail.com>
2026-08-07 10:28:04 -0400

    Update output.py to reference 'constants' instead of 'inputs'

--- packages/analysis_plugins/change_detection/src/mtf_change_detection/output.py
+++ packages/analysis_plugins/change_detection/src/mtf_change_detection/output.py
@@ -4,24 +4,24 @@ from mtf.output.console_utils import indentation
 from mtf.output.file_utils import FileReadType, FileSystemTree, list_to_txt
 
 
-def make_output(inputs: set[str], outputs: set[str]) -> tuple[str, FileSystemTree]:
+def make_output(constants: set[str], outputs: set[str]) -> tuple[str, FileSystemTree]:
     return (
-        _make_console_output(inputs, outputs),
-        _make_file_output(inputs, outputs),
+        _make_console_output(constants, outputs),
+        _make_file_output(constants, outputs),
     )
 
 
-def _make_console_output(inputs: set[str], outputs: set[str]) -> str:
+def _make_console_output(constants: set[str], outputs: set[str]) -> str:
     lines = []
 
     lines.append(
         f"Changes to {len(outputs)} outputs observed resulting from changes to "
-        f"{len(inputs)} inputs.\n"
+        f"{len(constants)} constants.\n"
     )
 
-    lines.append("Inputs:")
+    lines.append("Constants:")
 
-    for input in inputs:
+    for input in constants:
         lines.append(indentation.with_indentation(input, 1))
     
     lines.append("")
@@ -34,11 +34,11 @@ def _make_console_output(inputs: set[str], outputs: set[str]) -> str:
     return "\n".join(lines)
 
 
-def _make_file_output(inputs: set[str], outputs: set[str]) -> FileSystemTree:
+def _make_file_output(constants: set[str], outputs: set[str]) -> FileSystemTree:
     files = {
-        Path("inputs.txt"): (
+        Path("constants.txt"): (
             FileReadType.IN_MEMORY,
-            [list_to_txt.convert_to_txt_data(list(inputs))],
+            [list_to_txt.convert_to_txt_data(list(constants))],
         ),
         Path("outputs.txt"): (
             FileReadType.IN_MEMORY,
